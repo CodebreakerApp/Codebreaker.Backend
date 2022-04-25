e438fcd3086e94099f943dd4aecadbeec308833c
Christian Nagel <christian@christiannagel.com>
2022-04-25 09:39:29 +0200

    rethrow on exception

--- src/api/CodeBreaker.APIs/Data/CodeBreakerContext.cs
+++ src/api/CodeBreaker.APIs/Data/CodeBreakerContext.cs
@@ -47,6 +47,7 @@ internal class CodeBreakerContext : DbContext, ICodeBreakerContext
         catch (Exception ex)
         {
             _logger.LogError(ex, ex.Message);
+            throw;
         }
     }
 
