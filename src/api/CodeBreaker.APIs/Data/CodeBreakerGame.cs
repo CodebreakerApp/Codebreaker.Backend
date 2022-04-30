0f31f74192dbcf27c7c95b68d9ca605b2d6c2595
Christian Nagel <christian@christiannagel.com>
2022-04-30 16:40:25 +0200

    share report api models between data and dto

--- src/api/CodeBreaker.APIs/Data/CodeBreakerGame.cs
+++ src/api/CodeBreaker.APIs/Data/CodeBreakerGame.cs
@@ -1,12 +1,5 @@
 ﻿namespace CodeBreaker.APIs.Data;
 
-internal record CodeBreakerGame(string CodeBreakerGameId, string Code, string User, DateTime Time)
-{
-    public List<CodeBreakerMove> Moves { get; init; } = new();
-}
-
-internal record CodeBreakerMove(string CodeBreakerGameId, int MoveNumber, string Moves, string Keys);
-
 internal record CodeBreakerGameMove(string Id, string CodeBreakerGameId, int MoveNumber, string Move, DateTime Time, string Keys, string Code)
 {
     // constructor for the initial move
@@ -14,9 +7,3 @@ internal record CodeBreakerGameMove(string Id, string CodeBreakerGameId, int Mov
         : this(Id, CodeBreakerGameId, MoveNumber: 0, Move: string.Empty, Time, Keys: string.Empty, Code: string.Empty) { }
 }
 
-internal record GamesInformationDetail(DateTime Date)
-{
-    public List<CodeBreakerGame> Games { get; init; } = new List<CodeBreakerGame>();
-}
-
-internal record GamesInfo(DateTime Time, string User, int NumberMoves);
