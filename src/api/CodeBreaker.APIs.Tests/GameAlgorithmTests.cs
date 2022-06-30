119d5ae4e892dc31d87d8694247cbfeed177e548
Christian Nagel <christian@christiannagel.com>
2022-06-30 12:16:17 +0200

    update unit tests after api refactor #42

---
+++ src/api/CodeBreaker.APIs.Tests/GameAlgorithmTests.cs
@@ -0,0 +1,116 @@
+using CodeBreaker.APIs.Data;
+using CodeBreaker.APIs.Services;
+using CodeBreaker.Shared;
+
+using Microsoft.Extensions.Logging;
+
+using System.Collections;
+
+using Xunit;
+
+using static CodeBreaker.Shared.CodeBreakerColors;
+
+namespace CodeBreaker.APIs.Tests;
+
+public class TestLogger<T> : ILogger<T>, IDisposable
+{
+    public IDisposable? BeginScope<TState>(TState state) where TState: notnull => 
+        this;
+    
+    public void Dispose() { }
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+    }
+}
+
+public class GameAlgorithmTests
+{
+    [Fact]
+    public void SetMoveWithThreeWhite()
+    {
+        string[] expected = { White, White, White };
+        string[] code = { Green, Yellow, Green, Black };
+        string gameId = Guid.NewGuid().ToString();
+        
+        Game game = new(gameId, "Game6x4", "test", code);
+        GameAlgorithm algorithm = new GameAlgorithm(new TestLogger<GameAlgorithm>());
+
+        
+        GameMove guess = new(gameId, 1, new[] { Yellow, Green, Black, Blue });
+
+        var result = algorithm.SetMove(game, guess, 4);
+        var actual = result.Result.KeyPegs;
+        Assert.Equal(expected, actual);
+    }
+
+    [InlineData(Red, Yellow, Red, Blue, Black, White, White)]
+    [InlineData(White, White, Blue, Red, Black, Black)]
+    [Theory]
+    public void SetMoveUsingVariousData(string guess1, string guess2, string guess3, string guess4, params string[] expected)
+    {
+        string gameId = Guid.NewGuid().ToString();
+        string[] code = new[] { Red, Green, Blue, Red };
+        Game game = new(gameId, "Game6x4", "test", code);
+       
+
+        List<string> codePegs = new(new string[] { guess1, guess2, guess3, guess4 });
+        GameAlgorithm algorithm = new(new TestLogger<GameAlgorithm>());
+
+        GameMove guess = new(gameId, 1, codePegs);
+        (GameMoveResult gameResult, _, _) = algorithm.SetMove(game, guess, 4);
+        string[] result = gameResult.KeyPegs.ToArray();
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [ClassData(typeof(TestData))]
+    public void SetMoveUsingVariousData2(string[] code, string[] guesses, string[] expected)
+    {
+        string gameId = Guid.NewGuid().ToString();
+        Game game = new(gameId, "Game6x4", "test", code);
+        
+        GameMove guess = new(gameId, 1, guesses);
+
+        GameAlgorithm algorithm = new(new TestLogger<GameAlgorithm>());
+
+        var result = algorithm.SetMove(game, guess, 4);
+        string[] actual = result.Result.KeyPegs.ToArray();
+        Assert.Equal(expected, actual);
+    }
+}
+
+public class TestData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[]
+        {
+            new string[] { Green, Blue, Green, Yellow }, // code
+            new string[] { Green, Green, Black, White }, // inputdata
+            new string[] { Black, White } // results
+        };
+        yield return new object[]
+        {
+            new string[] { Red, Blue, Black, White }, // code
+            new string[] { Black, Black, Red, Yellow }, // inputdata
+            new string[] { White, White } // results
+        };
+        yield return new object[]
+        {
+            new string[] { Yellow, Black, Yellow, Green },
+            new string[] { Black, Black, Black, Black },
+            new string[] { Black }
+        };
+        yield return new object[]
+        {
+            new string[] { Yellow, Yellow, White, Red },
+            new string[] { Green, Yellow, White, Red },
+            new string[] { Black, Black, Black }
+        };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
