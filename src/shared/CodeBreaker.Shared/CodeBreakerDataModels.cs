0ce7c1b0d89d2d457db9e87be9e9b561558cef72
Christian Nagel <christian@christiannagel.com>
2022-06-30 14:08:11 +0200

    include change for move data model #42

--- src/shared/CodeBreaker.Shared/CodeBreakerDataModels.cs
+++ src/shared/CodeBreaker.Shared/CodeBreakerDataModels.cs
@@ -1,8 +1,10 @@
 ﻿namespace CodeBreaker.Shared;
 
+// EF Core model to store the game data
 public record CodeBreakerGame(string CodeBreakerGameId, string GameType, string Code, string User, DateTime Time)
 {  
     public List<CodeBreakerMove> Moves { get; init; } = new();
 }
 
-public record CodeBreakerMove(string CodeBreakerGameId, int MoveNumber, string Moves, string Keys);
+// EF Core model to store the game move data
+public record CodeBreakerMove(string CodeBreakerGameId, int MoveNumber, string Moves, string Keys, DateTime Time);
