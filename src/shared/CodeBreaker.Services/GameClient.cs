ed8b9b0d3028031df790fc82467ac7a2e28bb193
Christian Nagel <christian@christiannagel.com>
2022-07-10 10:32:45 +0200

    move gameclient from view-models library to the codebreaker services library #60

---
+++ src/shared/CodeBreaker.Services/GameClient.cs
@@ -0,0 +1,34 @@
+﻿using CodeBreaker.Shared.APIModels;
+
+using System.Net.Http.Json;
+
+namespace CodeBreaker.Services;
+
+public class GameClient
+{
+    private readonly HttpClient _httpClient;
+
+    public GameClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<CreateGameResponse> StartGameAsync(string name)
+    {
+        CreateGameRequest request = new(name);
+        var responseMessage = await _httpClient.PostAsJsonAsync("start/6x4", request);
+        responseMessage.EnsureSuccessStatusCode();
+        var response = await responseMessage.Content.ReadFromJsonAsync<CreateGameResponse>();
+        return response;
+    }
+
+    public async Task<(bool Completed, bool Won, string[] KeyPegs)> SetMoveAsync(string gameId, int moveNumber, params string[] colorNames)
+    {
+        MoveRequest moveRequest = new(gameId, moveNumber, colorNames);
+
+        var responseMessage = await _httpClient.PostAsJsonAsync("move/6x4", moveRequest);
+        responseMessage.EnsureSuccessStatusCode();
+        var response = await responseMessage.Content.ReadFromJsonAsync<MoveResponse>();
+        return (response.Completed, response.Won, response.KeyPegs?.ToArray() ?? new string[0]);
+    }
+}
