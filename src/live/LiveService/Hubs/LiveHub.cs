473d21748036998a38b91ff424ded0df86e181af
Sebastian <10291845+szv@users.noreply.github.com>
2022-07-18 10:45:32 +0200

    Added shared project for LiveService and added constant EventNames for SignalR-Events.

--- src/live/LiveService/Hubs/LiveHub.cs
+++ src/live/LiveService/Hubs/LiveHub.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.AspNetCore.SignalR;
+using static CodeBreaker.LiveService.Shared.LiveEventNames;
 
 namespace LiveService.Hubs;
 
@@ -11,17 +12,17 @@ public class LiveHub : Hub, ILiveHub
         Clients.Caller.SendAsync("test");
 
     public Task GameCreated(GameCreatedArgs args) =>
-        Clients.All.SendAsync("gameCreated", new GameCreatedPayload(args.Game));
+        Clients.All.SendAsync(GameCreatedEventName, new GameCreatedPayload(args.Game));
 
     public Task GameCancelled(GameCancelledArgs args) =>
-        Clients.All.SendAsync("gameCancelled", new GameCancelledPayload(args.Game));
+        Clients.All.SendAsync(GameCancelledEventName, new GameCancelledPayload(args.Game));
 
     public Task GameDeleted(GameDeletedArgs args) =>
-        Clients.All.SendAsync("gameDeleted", new GameDeletedPayload(args.Game));
+        Clients.All.SendAsync(GameDeletedEventName, new GameDeletedPayload(args.Game));
 
     public Task GameFinished(GameFinishedArgs args) =>
-        Clients.All.SendAsync("gameFinished", new GameFinishedPayload(args.Game));
+        Clients.All.SendAsync(GameFinishedEventName, new GameFinishedPayload(args.Game));
 
     public Task MoveCreated(MoveCreatedArgs args) =>
-        Clients.All.SendAsync("moveCreated", new MoveCreatedPayload(args.Game));
+        Clients.All.SendAsync(MoveCreatedEventName, new MoveCreatedPayload(args.Game));
 }
