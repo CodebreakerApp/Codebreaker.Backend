b59c325cf35d13fec3cfb20b0bfbc8e884aafaa6
Sebastian <10291845+szv@users.noreply.github.com>
2022-07-19 12:25:16 +0200

    Replaced EventGrid with EventHub and added SignalRStreaming to the LiveService.

---
+++ src/live/CodeBreaker.LiveService/EventSourceService.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using Azure.Messaging.EventHubs.Consumer;
+using CodeBreaker.LiveService.Shared;
+
+namespace CodeBreaker.LiveService;
+
+public class EventSourceService
+{
+    private readonly EventHubConsumerClient _eventHubConsumerClient;
+
+    public EventSourceService(IConfiguration configuration)
+    {
+        string consumerGroupName = EventHubConsumerClient.DefaultConsumerGroupName;
+        const string eventHubConnectionString = "Azure:EventHub:ConnectionString";
+        _eventHubConsumerClient = new EventHubConsumerClient(consumerGroupName, configuration[eventHubConnectionString] ?? throw new ArgumentNullException($"The \"{eventHubConnectionString}\" is not configured"));
+    }
+
+    public async IAsyncEnumerable<LiveHubArgs> SubscribeAsync([EnumeratorCancellation] CancellationToken token = default)
+    {
+        await foreach (PartitionEvent e in _eventHubConsumerClient.ReadEventsAsync(token))
+            yield return e.Data.EventBody.ToObjectFromJson<LiveHubArgs>();
+    }
+}
--- src/live/CodeBreaker.LiveService/LiveHub.cs
+++ src/live/CodeBreaker.LiveService/LiveHub.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using CodeBreaker.LiveService.Shared;
+using CodeBreaker.LiveService;
 
 namespace LiveService;
 
 public class LiveHub : Hub
 {
+    private readonly EventSourceService _eventSourceService;
+
+    public LiveHub(EventSourceService eventSourceService) =>
+        _eventSourceService = eventSourceService;
+
     public Task FireGameEvent(LiveHubArgs args, CancellationToken token) =>
         Clients.All.SendAsync("gameEvent", args, token);
+
+    public IAsyncEnumerable<LiveHubArgs> SubscribeToGameEvents(CancellationToken token) =>
+        _eventSourceService.SubscribeAsync(token);
 }
