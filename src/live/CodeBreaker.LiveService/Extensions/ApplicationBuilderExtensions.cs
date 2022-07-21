c041a5961d6872c1119c4e90beb2e1cdd0813b4e
Sebastian <10291845+szv@users.noreply.github.com>
2022-07-21 23:31:36 +0200

    Added IOptions, AzureAppConfig and AzureKeyVault

--- src/live/CodeBreaker.LiveService/Extensions/ApplicationBuilderExtensions.cs
+++ src/live/CodeBreaker.LiveService/Extensions/ApplicationBuilderExtensions.cs
@@ -1,30 +1,15 @@
 ﻿using CodeBreaker.LiveService.Options;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.Azure.SignalR;
 
 namespace CodeBreaker.LiveService.Extensions;
 
 public static class ApplicationBuilderExtensions
 {
-    public static ISignalRServerBuilder AddAzureSignalRIfConfigured(this ISignalRServerBuilder signalRServerBuilder, IConfiguration configuration, Action<ServiceOptions> options)
+    public static ISignalRServerBuilder AddAzureSignalRIfConfigured(this ISignalRServerBuilder signalRServerBuilder, LiveServiceOptions options)
     {
-        AzureOptions azureOptions = new AzureOptions();
-        configuration.GetSection(AzureOptions.Name).Bind(azureOptions);
-
-        if (string.IsNullOrEmpty(azureOptions?.SignalR?.ConnectionString))
+        if (string.IsNullOrEmpty(options.ConnectionStrings.SignalR))
             return signalRServerBuilder;
 
-        return signalRServerBuilder.AddAzureSignalR(options);
-    }
-
-    public static ISignalRServerBuilder AddAzureSignalRIfConfigured(this ISignalRServerBuilder signalRServerBuilder, IConfiguration configuration)
-    {
-        AzureOptions azureOptions = new AzureOptions();
-        configuration.GetSection(AzureOptions.Name).Bind(azureOptions);
-
-        if (string.IsNullOrEmpty(azureOptions?.SignalR?.ConnectionString))
-            return signalRServerBuilder;
-
-        return signalRServerBuilder.AddAzureSignalR();
+        return signalRServerBuilder.AddAzureSignalR(options.ConnectionStrings.SignalR);
     }
 }
--- src/live/CodeBreaker.LiveService/Program.cs
+++ src/live/CodeBreaker.LiveService/Program.cs
@@ -1,10 +1,47 @@
-using CodeBreaker.LiveService.Extensions;
+using Azure.Identity;
+using Azure.Messaging.EventHubs.Consumer;
 using CodeBreaker.LiveService.Options;
+using CodeBreaker.Shared.Exceptions;
 using LiveService;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Options;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSignalR().AddAzureSignalRIfConfigured(builder.Configuration);
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    string endpoint = builder.Configuration["AzureAppConfigurationEndpoint"] ?? throw new ConfigurationNotFoundException("AzureAppConfigurationEndpoint");
+    DefaultAzureCredential credential = new();
+    //AzureCliCredential credential = new();
+    options.Connect(new Uri(endpoint), credential)
+        .Select(KeyFilter.Any, LabelFilter.Null)
+        .Select(KeyFilter.Any, builder.Environment.EnvironmentName)
+        .ConfigureKeyVault(vault => vault.SetCredential(credential));
+});
+builder.Services.AddAzureAppConfiguration();
+builder.Services.Configure<LiveServiceOptions>(builder.Configuration.GetRequiredSection("LiveService"));
+builder.Services.AddSingleton(x => x.GetRequiredService<IOptions<LiveServiceOptions>>().Value);
+builder.Services.AddScoped(x =>
+{
+    LiveServiceOptions options = x.GetRequiredService<LiveServiceOptions>();
+    return new EventHubConsumerClient(
+        options.EventHub.ConsumerGroupName ?? EventHubConsumerClient.DefaultConsumerGroupName,
+        options.EventHub.ConnectionString ?? throw new ConfigurationNotFoundException(),
+        options.EventHub.Name ?? throw new ConfigurationNotFoundException(),
+        new DefaultAzureCredential()
+    );
+});
+string? signalRConnectionString = builder.Configuration["LiveService:ConnectionStrings:SignalR"];
+builder.Services.AddSignalR().AddAzureSignalR(signalRConnectionString);
 WebApplication app = builder.Build();
 
+app.UseAzureAppConfiguration();
 app.MapHub<LiveHub>("/live");
+//app.MapGet("/update-config", (
+//    IConfigurationRoot c,
+//    ILogger logger
+//) =>
+//{
+//    c.Reload();
+//    logger.LogInformation("Configuration reloaded");
+//});
 app.Run();
