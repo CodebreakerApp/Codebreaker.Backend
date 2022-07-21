7ecccd33fcc4f67ae81a4cda4f4a7fbd1c634961
Sebastian <10291845+szv@users.noreply.github.com>
2022-07-21 23:42:42 +0200

    Added reload-endpoint to LiveService.

--- src/live/CodeBreaker.LiveService/Program.cs
+++ src/live/CodeBreaker.LiveService/Program.cs
@@ -3,6 +3,7 @@ using Azure.Messaging.EventHubs.Consumer;
 using CodeBreaker.LiveService.Options;
 using CodeBreaker.Shared.Exceptions;
 using LiveService;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Options;
 
@@ -10,8 +11,8 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddAzureAppConfiguration(options =>
 {
     string endpoint = builder.Configuration["AzureAppConfigurationEndpoint"] ?? throw new ConfigurationNotFoundException("AzureAppConfigurationEndpoint");
-    DefaultAzureCredential credential = new();
-    //AzureCliCredential credential = new();
+    //DefaultAzureCredential credential = new();
+    AzureCliCredential credential = new();
     options.Connect(new Uri(endpoint), credential)
         .Select(KeyFilter.Any, LabelFilter.Null)
         .Select(KeyFilter.Any, builder.Environment.EnvironmentName)
@@ -36,12 +37,12 @@ WebApplication app = builder.Build();
 
 app.UseAzureAppConfiguration();
 app.MapHub<LiveHub>("/live");
-//app.MapGet("/update-config", (
-//    IConfigurationRoot c,
-//    ILogger logger
-//) =>
-//{
-//    c.Reload();
-//    logger.LogInformation("Configuration reloaded");
-//});
+app.MapGet("/update-config", (
+    [FromServices] IConfigurationRoot c,
+    [FromServices] ILogger<Program> logger
+) =>
+{
+    c.Reload();
+    logger.LogInformation("Configuration reloaded");
+});
 app.Run();
