42a5fac40ae2f60c8bb69c0f8397d1824c388766
Sebastian <10291845+szv@users.noreply.github.com>
2022-08-16 13:54:41 +0200

    Added ApplicationInsights to "Live".

---
+++ src/live/CodeBreaker.LiveService/ApplicationInsights/ApplicationInsightsTelemetryInitializer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace CodeBreaker.LiveService.ApplicationInsights;
+
+public class ApplicationInsightsTelemetryInitializer : ITelemetryInitializer
+{
+    public void Initialize(ITelemetry telemetry)
+    {
+        telemetry.Context.Cloud.RoleName = "CodeBreaker Live";
+        telemetry.Context.Cloud.RoleInstance = "Azure Container App";
+    }
+}
