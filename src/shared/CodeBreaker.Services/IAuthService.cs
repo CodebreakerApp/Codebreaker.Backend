ff2d3ba946445471be04065df08c3c20f1adfcbd
Sebastian <10291845+szv@users.noreply.github.com>
2022-08-24 20:01:14 +0200

    Fix IAuthService-Interface

--- src/shared/CodeBreaker.Services/IAuthService.cs
+++ src/shared/CodeBreaker.Services/IAuthService.cs
@@ -6,7 +6,5 @@ public interface IAuthService
 {
     Task<AuthenticationResult> LoginAsync(CancellationToken cancellation = default);
 
-    Task<AuthenticationResult> RegisterAsync(CancellationToken cancellation = default);
-
     Task<bool> IsAuthenticatedAsync(CancellationToken cancellation = default);
 }
