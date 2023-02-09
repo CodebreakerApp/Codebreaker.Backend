7a1c81349cf55d1c9e4394bf976e8153c3b00ca2
Sebastian <10291845+szv@users.noreply.github.com>
2023-02-09 16:57:34 +0100

    Locallized UserValidationService

--- src/services/user/CodeBreaker.UserService/Services/UserValidationService.cs
+++ src/services/user/CodeBreaker.UserService/Services/UserValidationService.cs
@@ -1,6 +1,7 @@
 ﻿using CodeBreaker.Shared.Models.Users;
 using FluentValidation;
 using FluentValidation.Results;
+using Microsoft.Extensions.Localization;
 
 namespace CodeBreaker.UserService.Services;
 
@@ -10,10 +11,13 @@ internal class UserValidationService : IUserValidationService
 
     private readonly IValidator<User> _userValidator;
 
-    public UserValidationService(IGamerNameService gamerNameService, IValidator<User> userValidator)
+    private readonly IStringLocalizer _localizer;
+
+    public UserValidationService(IGamerNameService gamerNameService, IValidator<User> userValidator, IStringLocalizer<UserValidationService> localizer)
     {
         _gamerNameService = gamerNameService;
         _userValidator = userValidator;
+        _localizer = localizer;
     }
 
     public async Task<ValidationResult> ValidateUserAsync(User user, CancellationToken cancellationToken = default)
@@ -21,7 +25,7 @@ internal class UserValidationService : IUserValidationService
         var result = _userValidator.Validate(user);
 
         if (result.IsValid && !await _gamerNameService.IsGamerNameTakenAsync(user.GamerName))
-            result.Errors.Add(new(nameof(user.GamerName), "The gamername is already taken.", user.GamerName)); // TODO Localize
+            result.Errors.Add(new(nameof(user.GamerName), _localizer["GamerNameTakenMessage"], user.GamerName));
 
         return result;
     }
