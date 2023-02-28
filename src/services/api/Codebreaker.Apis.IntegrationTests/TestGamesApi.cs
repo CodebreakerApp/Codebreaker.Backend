316ddfc094a8c4ffa61bd146e3db3162964cdd98
Christian Nagel <christian@christiannagel.com>
2023-02-28 16:38:53 +0100

    test game move start, a move, and delete

--- src/services/api/Codebreaker.Apis.IntegrationTests/TestGamesApi.cs
+++ src/services/api/Codebreaker.Apis.IntegrationTests/TestGamesApi.cs
@@ -44,24 +44,6 @@ public class TestGamesApi : PlaywrightTest
             return;
         }
 
-        // expecting about this JSON
-        //string expected = """
-        //    {
-        //        "gameTypes": [
-        //        {
-        //            "name": "8x5Game"
-        //        },
-        //        {
-        //            "name": "6x4MiniGame"
-        //        },
-        //        {
-        //            "name": "6x4Game"
-        //        }
-        //      ]
-        //    }
-        //    """;
-
-        // JsonElement jsonExpected = JsonDocument.Parse(expected).RootElement;
         var response = await _request.GetAsync($"{_baseUrl}/gametypes");
         Assert.That(response.Ok, Is.True);
 
@@ -78,5 +60,101 @@ public class TestGamesApi : PlaywrightTest
             if (gameTypeName == "6x4Game") match = true;  // at least this game type should be available
         }
         Assert.That(match, Is.True);
+    }
+
+
+    [Test]
+    public async Task ShouldCreateA6x4GameAndReturnMoveResult()
+    {
+        if (_request is null)
+        {
+            Assert.Fail();
+            return;
+        }
+
+        string username = "testuser";
+
+        Dictionary<string, string> request = new()
+        {
+            ["username"] = username,
+            ["gameType"] = "6x4Game"
+        };
+
+        // JsonElement jsonExpected = JsonDocument.Parse(expected).RootElement;
+        var response = await _request.PostAsync($"{_baseUrl}/games", new()
+        {
+            DataObject = request
+        });
+        Assert.That(response.Ok, Is.True);
+
+        var json = await response.JsonAsync();
+        Assert.DoesNotThrow(() =>
+        {
+            json.Value.GetProperty("game");
+            json.Value.GetProperty("game").GetProperty("type").GetProperty("name");
+        });
+
+        JsonElement jsonGame = json.Value.GetProperty("game");
+        JsonElement jsonType = jsonGame.GetProperty("type");
+
+        // is the game type the same as requested?
+        Assert.That(jsonType.GetProperty("name").ToString(), Is.EqualTo("6x4Game"));
+
+        Guid gameId = jsonGame.GetProperty("gameId").GetGuid();
+
+        int holes = int.Parse(jsonType.GetProperty("holes").ToString());
+
+        int maxMoves = int.Parse(jsonType.GetProperty("maxMoves").ToString());
+
+        int length = jsonGame.GetProperty("moves").GetArrayLength();
+
+        Assert.Multiple(() =>
+        {
+            // holes should be 4 with this game type
+            Assert.That(holes, Is.EqualTo(4));
+
+            // max moves should be 12 with this game type
+            Assert.That(maxMoves, Is.EqualTo(12));
+
+            // username returned should contain the username requested
+            Assert.That(jsonGame.GetProperty("username").ToString(), Is.EqualTo(username));
+
+            // moves should be an empty array
+            Assert.That(length, Is.EqualTo(0));
+        });
+
+        Dictionary<string, object> moveRequest = new()
+        {
+            ["guessPegs"] = new string[] { "Red", "Blue", "Red", "Blue" }
+        };
+
+        response = await _request.PostAsync($"{_baseUrl}/games/{gameId}/moves", new()
+        {
+            DataObject = moveRequest
+        });
+
+        Assert.That(response.Ok, Is.True);
+
+        json = await response.JsonAsync();
+        JsonElement keyPegs = json.Value.GetProperty("keyPegs");
+        int blackPegs = keyPegs.GetProperty("black").GetInt32();
+        int whitePegs = keyPegs.GetProperty("white").GetInt32();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(blackPegs, Is.LessThan(5));
+            Assert.That(whitePegs, Is.LessThan(5));
+        });
+
+        Dictionary<string, bool> deleteRequest = new()
+        {
+            ["cancel"] = false
+        };
+        response = await _request.DeleteAsync($"{_baseUrl}/games/{gameId}", new()
+        {
+            DataObject = deleteRequest
+        });
+
+        Assert.That(response.Ok, Is.True);
     }
 }
