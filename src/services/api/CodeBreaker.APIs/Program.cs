4f07c0a2a6d39ac7d24a818a6fba076aa8522b21
Christian Nagel <christian@christiannagel.com>
2023-02-28 09:09:13 +0100

    JSON source generator enabled only with .NET 8 #383

--- src/services/api/CodeBreaker.APIs/Program.cs
+++ src/services/api/CodeBreaker.APIs/Program.cs
@@ -63,11 +63,13 @@ builder.Services.AddAzureAppConfiguration();
 builder.Services.Configure<ApiServiceOptions>(builder.Configuration.GetSection("ApiService"));
 builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<ApiServiceOptions>>().Value);
 
-// JSON Serialization
+#if NET8_0_OR_GREATER
+// JSON Serialization - do not enable this before .NET 8
 builder.Services.Configure<JsonOptions>(options =>
 {
     options.SerializerOptions.AddContext<GamesJsonSerializerContext>();
 });
+#endif
 
 // ApplicationInsights
 builder.Services.AddApplicationInsightsTelemetry();
