cc0609e796fb5b9ec972b4fbbef1af61b6630fe3
Sebastian <10291845+szv@users.noreply.github.com>
2023-03-04 14:16:22 +0100

    Replaced ConfigurationNotFoundException in CodeBreaker.Shared

--- src/shared/CodeBreaker.Shared/Extensions/ConfigExtensions.cs
+++ src/shared/CodeBreaker.Shared/Extensions/ConfigExtensions.cs
@@ -1,10 +1,7 @@
-﻿using CodeBreaker.Shared.Exceptions;
-using Microsoft.Extensions.Configuration;
-
-namespace Microsoft.Extensions.Configuration;
+﻿namespace Microsoft.Extensions.Configuration;
 
 public static class ConfigExtensions
 {
     public static string GetRequired(this IConfiguration config, string configKey) =>
-        config[configKey] ?? throw new ConfigurationNotFoundException(configKey);
+        config[configKey] ?? throw new InvalidOperationException($"Could not find configuration with key: \"{configKey}\"");
 }
