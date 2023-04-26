40abad5df1d731b966370f1a90f67a7cc924693f
Sebastian <10291845+szv@users.noreply.github.com>
2023-04-26 17:12:26 +0200

    Created CodeBreaker.Data.Common.Cosmos

---
+++ src/services/shared/CodeBreaker.Data.Common.Cosmos/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.EntityFrameworkCore;
+
+public static class IQueryableExtensions
+{
+    public static IQueryable<T> WithPartitionKey<T>(this IQueryable<T> queryable, Guid partitionKey)
+        where T : class =>
+        queryable.WithPartitionKey(partitionKey.ToString());
+}
