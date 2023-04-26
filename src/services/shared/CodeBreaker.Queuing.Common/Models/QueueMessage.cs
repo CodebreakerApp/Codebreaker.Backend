d5da61d5a566f660a713328fe7853bc529ec28ce
Sebastian <10291845+szv@users.noreply.github.com>
2023-04-26 17:12:50 +0200

    Created CodeBreaker.Queueing.Common

---
+++ src/services/shared/CodeBreaker.Queuing.Common/Models/QueueMessage.cs
@@ -0,0 +1,32 @@
+﻿namespace CodeBreaker.Queuing.Common.Models;
+
+public class QueueMessage<TBody> : IEquatable<QueueMessage<TBody>>
+{
+    public QueueMessage(string id, string popReceipt, TBody body)
+    {
+        Id = id;
+        PopReceipt = popReceipt;
+        Body = body;
+    }
+
+    public string Id { get; init; }
+
+    public string PopReceipt { get; init; }
+
+    public TBody Body { get; init; }
+
+    public bool Equals(QueueMessage<TBody>? other) =>
+        Id == other?.Id;
+
+    public override bool Equals(object? obj) =>
+        Equals(obj as QueueMessage<TBody>);
+
+    public override int GetHashCode() =>
+        Id.GetHashCode();
+
+    public static bool operator ==(QueueMessage<TBody> a, QueueMessage<TBody> b) =>
+        a.Equals(b);
+
+    public static bool operator !=(QueueMessage<TBody> a, QueueMessage<TBody> b) =>
+        !a.Equals(b);
+}
