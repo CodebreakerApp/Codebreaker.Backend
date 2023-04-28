70c758fc640fbb2d4caaec7c48c95cd1fad4ee21
Sebastian <10291845+szv@users.noreply.github.com>
2023-04-28 14:32:09 +0200

    Added QueueWorker

---
+++ src/services/report/CodeBreaker.ReportService.QueueWorker/Mapping/QueueDtoMapping.cs
@@ -0,0 +1,28 @@
+﻿using CodeBreaker.Data.ReportService.Models;
+using CodeBreaker.Queuing.ReportService.Transfer;
+
+namespace CodeBreaker.ReportService.QueueWorker.Mapping;
+
+internal static class QueueDtoMapping
+{
+    public static Game ToModel(this GameDto game) =>
+        new()
+        {
+            Id = game.Id,
+            Type = game.Type,
+            Username = game.Username,
+            Start = game.Start,
+            End = game.End,
+            Moves = game.Moves?.Select(ToModel) as IReadOnlyCollection<Move> ?? Array.Empty<Move>()
+        };
+
+    public static Move ToModel(this MoveDto move) =>
+        new()
+        {
+            GuessPegs = move.GuessPegs,
+            KeyPegs = move.KeyPegs.ToModel()
+        };
+
+    public static KeyPegs ToModel(this KeyPegsDto keyPegs) =>
+        new(keyPegs.Black, keyPegs.White);
+}
