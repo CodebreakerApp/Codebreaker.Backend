819b248c3577cd002bbe25658d316c5d9fbe07c6
Christian Nagel <christian@christiannagel.com>
2023-05-08 18:32:57 +0200

    use strongly typed ColumnNames

--- src/services/gameapi/Codebreaker.GameAPIs.Data.Cosmos/Data/GameConfiguration.cs
+++ src/services/gameapi/Codebreaker.GameAPIs.Data.Cosmos/Data/GameConfiguration.cs
@@ -4,15 +4,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Codebreaker.GameAPIs.Data.Cosmos.Data;
+
 internal class GameConfiguration<T> : IEntityTypeConfiguration<T>
     where T : Game
 {
-    private const string StartDate = nameof(StartDate);
-
     public void Configure(EntityTypeBuilder<T> builder)
     {
-        builder.Property<string>(StartDate);
+        builder.Property<string>(ColumnNames.StartDate);
 
-        builder.HasPartitionKey(StartDate);
+        builder.HasPartitionKey(ColumnNames.StartDate);
     }
 }
