16ff8299cb0ab75ff94f6532ce4a5a83907f3501
Christian Nagel <christian@christiannagel.com>
2023-05-08 18:32:17 +0200

    CreateXX instead of GetXX methods

--- src/services/gameapi/Codebreaker.GameAPIs.Models/Factories/GamesFactory.cs
+++ src/services/gameapi/Codebreaker.GameAPIs.Models/Factories/GamesFactory.cs
@@ -13,25 +13,24 @@ public static class GamesFactory
     {
         static string GetRandomValue(string[] items) => items[Random.Shared.Next(items.Length)];
 
-        static SimpleGame Get6x4SimpleGame(GameType gameType, string playerName) =>
+        static SimpleGame Create6x4SimpleGame(GameType gameType, string playerName) =>
             new (Guid.NewGuid(), gameType, playerName, DateTime.Now, 4, 12)
             {
                 Codes = Enumerable.Range(0, 4).Select(i => new ColorField(GetRandomValue(s_colors6))).ToArray()
             };
 
-        static ColorGame Get6x4Game(GameType gameType, string playerName) =>
+        static ColorGame Create6x4Game(GameType gameType, string playerName) =>
             new(Guid.NewGuid(), gameType, playerName, DateTime.Now, 4, 12)
             {
                 Codes = Enumerable.Range(0, 4).Select(i => new ColorField(GetRandomValue(s_colors6))).ToArray()
             };
-
-        static ColorGame Get8x5Game(GameType gameType, string playerName) =>
-            new(Guid.NewGuid(), gameType, playerName, DateTime.Now, 5, 14)
+        static ColorGame Create8x5Game(GameType gameType, string playerName) =>
+            new(Guid.NewGuid(), gameType, playerName, DateTime.Now, 5, 12)
             {
                 Codes = Enumerable.Range(0, 5).Select(i => new ColorField(GetRandomValue(s_colors8))).ToArray()
             };
 
-        static ShapeGame Get5x5x4Game(GameType gameType, string playerName) =>
+        static ShapeGame Create5x5x4Game(GameType gameType, string playerName) =>
             new(Guid.NewGuid(), gameType, playerName, DateTime.Now, 4, 14)
             {
                 Codes = Enumerable.Range(0, 4).Select(i => new ShapeAndColorField(GetRandomValue(s_shapes5), GetRandomValue(s_colors5))).ToArray()
@@ -39,10 +38,10 @@ public static class GamesFactory
 
         return gameType switch
         {
-            GameType.Game6x4Mini => Get6x4SimpleGame(gameType, playerName),
-            GameType.Game6x4 => Get6x4Game(gameType, playerName),
-            GameType.Game8x5 => Get8x5Game(gameType, playerName),
-            GameType.Game5x5x4 => Get5x5x4Game(gameType, playerName),
+            GameType.Game6x4Mini => Create6x4SimpleGame(gameType, playerName),
+            GameType.Game6x4 => Create6x4Game(gameType, playerName),
+            GameType.Game8x5 => Create8x5Game(gameType, playerName),
+            GameType.Game5x5x4 => Create5x5x4Game(gameType, playerName),
             _ => throw new InvalidGameException("Invalid game type") { HResult = 4000 }
         };
     }
