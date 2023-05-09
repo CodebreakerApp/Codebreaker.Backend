1e48ed73171eb8d2efeef0df8b38784a11da2026
Christian Nagel <christian@christiannagel.com>
2023-05-09 10:54:04 +0200

    move core types to algorithm library

--- src/services/gameapi/Codebreaker.GameAPIs.Algorithms/Fields/ColorField.cs
+++ src/services/gameapi/Codebreaker.GameAPIs.Algorithms/Fields/ColorField.cs
@@ -3,4 +3,6 @@
 public partial record class ColorField(string Color)
 {
     public override string ToString() => Color;
+
+    public static implicit operator ColorField(string color) => new(color);
 }
