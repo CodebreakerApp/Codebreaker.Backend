9620e4a7939382a7ecfbdae5b6c40a5823d9d79d
Christian Nagel <christian@christiannagel.com>
2023-05-22 03:52:19 -0700

    override Move.ToString for logging implementation with abstract base class to return a non-nullable string

--- src/services/gameapi/Codebreaker.GameAPIs.Models/Move.cs
+++ src/services/gameapi/Codebreaker.GameAPIs.Models/Move.cs
@@ -7,6 +7,8 @@ public abstract class Move(Guid gameId, Guid moveId, int moveNumber)
     public Guid GameId { get; private set; } = gameId;
     public Guid MoveId { get; private set; } = moveId;
     public int MoveNumber { get; set; } = moveNumber;
+
+    public override string ToString() => $"{GameId}: {MoveNumber}";
 }
 
 public class Move<TField, TResult>(Guid GameId, Guid MoveId, int MoveNumber)
