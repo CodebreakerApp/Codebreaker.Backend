652f624740e2a4d699e67e2300852c8ec7843acb
Christian Nagel <christian@christiannagel.com>
2023-07-26 14:04:31 +0200

    add unit test for StartGame6x4 #9

---
+++ src/Codebreaker.GameAPIs.Client.Tests/TestGamesClient.cs
@@ -0,0 +1,82 @@
+using System.Net;
+
+using Microsoft.Extensions.Configuration;
+
+using Moq;
+using Moq.Protected;
+
+using Xunit.Abstractions;
+
+namespace Codebreaker.GameAPIs.Client.Tests;
+
+public class TestGamesClient
+{
+    private readonly ITestOutputHelper _outputHelper;
+
+    public TestGamesClient(ITestOutputHelper outputHelper)
+    {
+        _outputHelper = outputHelper;
+    }
+
+    [Fact]
+    public async Task TestStartGame6x4Async()
+    {
+        // Arrange
+        var configMock = new Mock<IConfiguration>();
+        configMock.Setup(x => x[It.IsAny<string>()]).Returns("http://localhost:5000");
+
+        Mock<HttpMessageHandler> handlerMock = new (MockBehavior.Strict);
+        string returnMessage = """
+        {
+            "gameId": "af8dd39f-6e16-41ef-9155-dcd3cf081e87",
+            "gameType": "Game6x4",
+            "playerName": "test",
+            "numberCodes": 4,
+            "maxMoves": 12,
+            "fieldValues": {
+                "colors": [
+                    "Red",
+                    "Green",
+                    "Blue",
+                    "Yellow",
+                    "Purple",
+                    "Orange"
+                ]
+            }
+        }
+        """;
+        handlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(returnMessage)
+            }).Verifiable();
+
+        HttpClient httpClient = new(handlerMock.Object)
+        {
+            BaseAddress = new System.Uri(configMock.Object["GameAPIs"] ?? throw new InvalidOperationException())
+        };
+
+        var gamesClient = new GamesClient(httpClient);
+
+        // Act
+        var response = await gamesClient.StartGameAsync(Models.GameType.Game6x4, "test");
+
+        // Assert
+        Assert.Equal(4, response.NumberCodes);
+        Assert.Equal(12, response.MaxMoves);
+        Assert.Single(response.FieldValues.Keys);
+        Assert.Equal("colors", response.FieldValues.Keys.First());
+        Assert.Equal(6, response.FieldValues["colors"].Length);
+
+        handlerMock.Protected().Verify(
+        "SendAsync",
+            Times.Once(),
+            ItExpr.IsAny<HttpRequestMessage>(),
+            ItExpr.IsAny<CancellationToken>());
+    }
+}
