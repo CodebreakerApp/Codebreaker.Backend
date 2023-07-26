b9fec6c944355f657fabae1288fbc4070fb63447
Christian Nagel <christian@christiannagel.com>
2023-07-26 09:56:33 +0200

    add documentation to model types #9

--- src/clients/common/Codebreaker.GameAPIs.Client/Models/CreateGameResponse.cs
+++ src/clients/common/Codebreaker.GameAPIs.Client/Models/CreateGameResponse.cs
@@ -1,5 +1,13 @@
 ﻿namespace Codebreaker.GameAPIs.Client.Models;
 
+/// <summary>
+/// Information about the game that was created
+/// </summary>
+/// <param name="GameId">The unique identifier of the game. This is needed to reference the game.</param>
+/// <param name="GameType">The game type with one of the <see cref="GameType"/>enum values</param>
+/// <param name="PlayerName">The name of the player</param>
+/// <param name="NumberCodes">The number of the codes the player needs to fill</param>
+/// <param name="MaxMoves">The maximum number of moves the game ends when its not solved</param>
 public record class CreateGameResponse(
     Guid GameId,
     GameType GameType,
@@ -7,5 +15,8 @@ public record class CreateGameResponse(
     int NumberCodes,
     int MaxMoves)
 {
+    /// <summary>
+    /// The possible field values for a game move
+    /// </summary>
     public required IDictionary<string, string[]> FieldValues { get; init; }
 }
--- src/clients/common/Codebreaker.GameAPIs.Client/Models/Game.cs
+++ src/clients/common/Codebreaker.GameAPIs.Client/Models/Game.cs
@@ -1,5 +1,14 @@
 ﻿namespace Codebreaker.GameAPIs.Client.Models;
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="gameId">The unique identifier of the game</param>
+/// <param name="gameType"></param>
+/// <param name="playerName"></param>
+/// <param name="startTime"></param>
+/// <param name="numberCodes"></param>
+/// <param name="maxMoves"></param>
 public class Game(
     Guid gameId,
     string gameType,
@@ -8,21 +17,74 @@ public class Game(
     int numberCodes,
     int maxMoves)
 {
+    /// <summary>
+    /// Gets the unique identifier of the game.
+    /// </summary>
     public Guid GameId { get; private set; } = gameId;
+
+    /// <summary>
+    /// Gets the type of the game. <see cref="GameType"/>
+    /// </summary>
     public string GameType { get; private set; } = gameType;
+
+    /// <summary>
+    /// Gets the name of the player.
+    /// </summary>
     public string PlayerName { get; private set; } = playerName;
+
+    /// <summary>
+    /// Gets information if the player was authenticated while playing the game.
+    /// </summary>
     public bool PlayerIsAuthenticated { get; set; } = false;
+
+    /// <summary>
+    /// Gets the start time of the game
+    /// </summary>
     public DateTime StartTime { get; private set; } = startTime;
+
+    /// <summary>
+    /// Gets the end time of the game or null if it did not end yet. This value is set from a game guess anylzer after the game was ended.
+    /// </summary>
     public DateTime? EndTime { get; set; }
+
+    /// <summary>
+    /// Gets the duration of the game or null if it did not end yet
+    /// </summary>
     public TimeSpan? Duration { get; set; }
+
+    /// <summary>
+    /// Gets the last move number. This number is set from an game move analyer after the move was set.
+    /// </summary>  
     public int LastMoveNumber { get; set; } = 0;
+
+    /// <summary>
+    /// Gets the number of codes the player needs to fill.
+    /// </summary>  
     public int NumberCodes { get; private set; } = numberCodes;
+
+    /// <summary>
+    /// Gets the maximum number of moves the game ends when its not solved.
+    /// </summary>
     public int MaxMoves { get; private set; } = maxMoves;
+
+    /// <summary>
+    /// Did the player win the game?
+    /// </summary>  
     public bool IsVictory { get; set; } = false;
 
+    /// <summary>
+    /// A list of possible field values the user has to chose from
+    /// </summary>
     public required IDictionary<string, IEnumerable<string>> FieldValues { get; init; }
 
+    /// <summary>
+    /// This is the solution of the game with string representations of the codes
+    /// </summary>
     public required string[] Codes { get; init; }
+
+    /// <summary>
+    /// A list of moves the player made
+    /// </summary>
     public ICollection<Move> Moves { get; init; } = new List<Move>();
 
     public override string ToString() => $"{GameId}:{GameType} - {StartTime}";
