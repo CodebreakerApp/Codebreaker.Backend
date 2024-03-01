eba8b0261418b90c6e9c66e556474be5859b331f
Christian Nagel <christian@christiannagel.com>
2024-03-01 10:46:41 +0100

    GameSummary type #159

---
+++ src/services/gameapi/Codebreaker.GameAPIs.Models/GameSummary.cs
@@ -0,0 +1,13 @@
+﻿namespace Codebreaker.GameAPIs.Models;
+
+public record class GameSummary(
+    Guid Id,
+    string GameType,
+    string PlayerName,
+    bool IsCompleted,
+    bool IsVictory,
+    DateTime StartTime,
+    TimeSpan Duration)
+{
+    public override string ToString() => $"{Id}:{GameType}, victory: {IsVictory}, duration: {Duration}";
+}
