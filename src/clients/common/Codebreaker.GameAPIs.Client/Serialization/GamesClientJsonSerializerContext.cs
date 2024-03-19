8036302fb194c2fc4ecc2738365b3e52648c5f72
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-03-19 16:46:19 +0100

    Created JsonSerializerContext for GamesClient

---
+++ src/clients/common/Codebreaker.GameAPIs.Client/Serialization/GamesClientJsonSerializerContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Codebreaker.GameAPIs.Client.Serialization;
+
+[JsonSourceGenerationOptions(
+    AllowTrailingCommas = true,
+    PropertyNameCaseInsensitive = true,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    UseStringEnumConverter = true
+)]
+[JsonSerializable(typeof(CreateGameRequest))]
+[JsonSerializable(typeof(CreateGameResponse))]
+[JsonSerializable(typeof(GameInfo))]  // MoveInfo is contained by GameInfo
+[JsonSerializable(typeof(GamesQuery))]
+[JsonSerializable(typeof(UpdateGameRequest))]
+[JsonSerializable(typeof(UpdateGameResponse))]
+internal partial class GamesClientJsonSerializerContext : JsonSerializerContext
+{
+}
