d982598b86ae55c827899f255a3017de38e160bf
Christian Nagel <christian@christiannagel.com>
2024-04-15 20:34:12 +0200

    game summary #174

--- src/services/gameapi/Codebreaker.GameAPIs.Models/GameSummary.cs
+++ src/services/gameapi/Codebreaker.GameAPIs.Models/GameSummary.cs
@@ -1,14 +1,22 @@
 ﻿namespace Codebreaker.GameAPIs.Models;
 
-public record class GameSummary(
-    Guid Id,
-    string GameType,
-    string PlayerName,
-    bool IsCompleted,
-    bool IsVictory,
-    int NumberMoves,
-    DateTime StartTime,
-    TimeSpan Duration)
+public class GameSummary(
+    Guid id,
+    string gameType,
+    string playerName,
+    bool isCompleted,
+    bool isVictory,
+    int numberMoves,
+    DateTime startTime,
+    TimeSpan duration)
 {
+    public Guid Id { get; private set; } = id;
+    public string GameType { get; private set; } = gameType;
+    public string PlayerName { get; private set; } = playerName;
+    public bool IsCompleted { get; private set; } = isCompleted;
+    public bool IsVictory { get; private set; } = isVictory;
+    public int NumberMoves { get; private set; } = numberMoves;
+    public DateTime StartTime { get; private set; } = startTime;
+    public TimeSpan Duration { get; private set; } = duration;
     public override string ToString() => $"{Id}:{GameType}, victory: {IsVictory}, duration: {Duration}";
 }
