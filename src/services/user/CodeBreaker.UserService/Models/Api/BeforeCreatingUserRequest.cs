8784ae09e766719e9b55f1f2a2cb2831f7f4cb92
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-05-13 03:11:09 +0200

    Specified JsonPropertyNames in BeforeCreatingUserRequest class

--- src/services/user/CodeBreaker.UserService/Models/Api/BeforeCreatingUserRequest.cs
+++ src/services/user/CodeBreaker.UserService/Models/Api/BeforeCreatingUserRequest.cs
@@ -1,11 +1,15 @@
-﻿namespace CodeBreaker.UserService.Models.Api;
+﻿using System.Text.Json.Serialization;
+
+namespace CodeBreaker.UserService.Models.Api;
 
 internal class BeforeCreatingUserRequest
 {
     public string Step { get; set; } = string.Empty;
 
+    [JsonPropertyName("client_id")]
     public string ClientId { get; set; } = string.Empty;
 
+    [JsonPropertyName("ui_locales")]
     public string UiLocales { get; set; } = string.Empty;
 
     //
@@ -15,11 +19,15 @@ internal class BeforeCreatingUserRequest
 
     public string Email { get; set; } = string.Empty;
 
-    public string DisplayName { get; set; } = string.Empty;
+    //public string DisplayName { get; set; } = string.Empty;
 
     public string GivenName { get; set; } = string.Empty;
 
     public string Surname { get; set; } = string.Empty;
 
-    public string Extension_dd21590c971e431494da34e2a8d47cce_GamerName { get; set; } = string.Empty;
+    [JsonPropertyName("extension_69ecacfaed14473ba27f9937b5195afb_GamerName")]
+    public string GamerName { get; set; } = string.Empty;
+
+    //[JsonPropertyName("extension_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxb_GamerName")]
+    //private string GamerName2 { set => GamerName = value; }
 }
