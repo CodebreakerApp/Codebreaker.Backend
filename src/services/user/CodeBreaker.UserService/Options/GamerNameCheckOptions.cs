b36645c5a2f960092a398092053b98b8297aa84e
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-05-14 14:40:32 +0200

    Removed unused property from GamerNameCheckOptions

--- src/services/user/CodeBreaker.UserService/Options/GamerNameCheckOptions.cs
+++ src/services/user/CodeBreaker.UserService/Options/GamerNameCheckOptions.cs
@@ -7,6 +7,4 @@ internal class GamerNameCheckOptions
     public string ClientId { get; set; } = string.Empty;
 
     public string ClientSecret { get; set; } = string.Empty;
-
-    public string GamerNameAttributeKey { get; set; } = string.Empty;
 }
