989d77be22a4aea26e1529d9eb808a7b55213f3c
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-05-14 14:31:33 +0200

    Defined validator for BeforeCreatingUserRequest dto

---
+++ src/services/user/CodeBreaker.UserService/Validators/BeforeCreatingUserRequestValidator.cs
@@ -0,0 +1,64 @@
+﻿using Azure.Identity;
+using CodeBreaker.UserService.Models.Api;
+using CodeBreaker.UserService.Options;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using Microsoft.Graph;
+
+namespace CodeBreaker.UserService.Validators;
+
+internal class BeforeCreatingUserRequestValidator : AbstractValidator<BeforeCreatingUserRequest>, IDisposable
+{
+    private readonly GraphServiceClient _graphServiceClient;
+
+    private readonly string _gamerNameAttributeKey;
+
+    public BeforeCreatingUserRequestValidator(IOptions<GamerNameCheckOptions> gamerNameCheckOptions, IStringLocalizer<BeforeCreatingUserRequestValidator> localizer)
+    {
+        // Setup
+        _gamerNameAttributeKey = gamerNameCheckOptions.Value.GamerNameAttributeKey;
+        string[] scopes = ["https://graph.microsoft.com/.default"];
+        TokenCredentialOptions options = new() { AuthorityHost = AzureAuthorityHosts.AzurePublicCloud };
+        ClientSecretCredential clientSecretCredential = new(gamerNameCheckOptions.Value.TenantId, gamerNameCheckOptions.Value.ClientId, gamerNameCheckOptions.Value.ClientSecret, options);
+        _graphServiceClient = new(clientSecretCredential, scopes);
+
+        // Validation rules
+        RuleFor(x => x.Email)
+            .EmailAddress()
+            .NotEmpty()
+            .MinimumLength(7)
+            .MaximumLength(200);
+        RuleFor(x => x.GamerName)
+            .NotEmpty()
+            .MinimumLength(3)
+            .MaximumLength(100)
+            .MustAsync(DoesGamerNameExistAsync)
+            .WithMessage(localizer["GamerNameTakenMessage"]);
+        RuleFor(x => x.GivenName)
+            .NotEmpty()
+            .MinimumLength(2)
+            .MaximumLength(100);
+        RuleFor(x => x.Surname)
+            .NotEmpty()
+            .MinimumLength(2)
+            .MaximumLength(100);
+    }
+
+    public void Dispose()
+    {
+        _graphServiceClient.Dispose();
+    }
+
+    private async Task<bool> DoesGamerNameExistAsync(string gamerName, CancellationToken cancellationToken)
+    {
+        var response = await _graphServiceClient.Users
+        .GetAsync(builder =>
+        {
+            builder.QueryParameters.Top = 1;
+                builder.QueryParameters.Select = ["id"];
+                builder.QueryParameters.Filter = $"{_gamerNameAttributeKey} eq '{gamerName}'";
+            }, cancellationToken);
+        return response?.Value?.Count == 0;
+    }
+}
