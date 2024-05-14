d472dd2b9255caffc8673ad1c9df49f06c37a898
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-05-14 14:32:49 +0200

    Removed old service-classes

--- src/services/user/CodeBreaker.UserService/Services/IUserValidationService.cs
+++
@@ -1,8 +0,0 @@
-﻿using CodeBreaker.Shared.Models.Users;
-using FluentValidation.Results;
-
-namespace CodeBreaker.UserService.Services;
-internal interface IUserValidationService
-{
-    Task<ValidationResult> ValidateUserAsync(User user, CancellationToken cancellationToken = default);
-}
