1ce74a5552bef9c701797061a712ccdd7de34a80
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-05-14 14:31:04 +0200

    Added Options class for gamer name suggestions

--- src/services/user/CodeBreaker.UserService/Options/GamerNameSuggestionOptions.cs
+++ src/services/user/CodeBreaker.UserService/Options/GamerNameSuggestionOptions.cs
@@ -5,4 +5,17 @@ internal class GamerNameSuggestionOptions
     public int MinimumNumber { get; set; } = 1;
 
     public int MaximumNumber { get; set; } = 1000;
+
+    public NameParts GamerNameParts { get; set; } = new ();
+}
+
+internal struct NameParts
+{
+    public NameParts()
+    {
+    }
+
+    public string[] First { get; set; } = [];
+
+    public string[] Second { get; set; } = [];
 }
