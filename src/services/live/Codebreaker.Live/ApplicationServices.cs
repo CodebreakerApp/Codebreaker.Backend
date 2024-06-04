777f748a7d4727a228e9bc1228e9d2ce94755bf7
Christian Nagel <christian@christiannagel.com>
2024-06-04 16:05:08 +0200

    #187 live service

---
+++ src/services/live/Codebreaker.Live/ApplicationServices.cs
@@ -0,0 +1,39 @@
+﻿namespace Codebreaker.Live;
+
+public static class ApplicationServices
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        var signalRBuilder = builder.Services.AddSignalR()
+            .AddMessagePackProtocol();
+
+        if (builder.Configuration["StartupMode"] is not "OnPremises")
+        {
+            signalRBuilder.AddNamedAzureSignalR("signalr");
+        }
+
+        builder.AddAzureEventHubConsumerClient("codebreakerevents",
+            settings =>
+            {
+                settings.EventHubName = "games";
+            });
+
+        builder.Services.AddSingleton<LiveHubClientsCount>();
+
+        // builder.Services.AddGrpc();
+    }
+
+    public static WebApplication MapApplicationEndpoints(this WebApplication app)
+    {
+        // map REST endpoints
+        // app.MapLiveGamesEndpoints(logger);
+        // map gRPC endpoints
+        // app.MapGrpcService<GRPCLiveGameService>();
+
+        // map SignalR hub
+        app.MapHub<LiveHub>("/livesubscribe");
+        app.MapHub<StreamingLiveHub>("/streaminglivesubscribe");
+
+        return app;
+    }
+}
