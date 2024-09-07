54798ee27529a243fdc21753f211a481f9c5b085
Christian Nagel <christian@christiannagel.com>
2024-09-07 13:20:44 +0200

    fix 5x5x4 algorithm #235

--- src/services/common/Codebreaker.GameAPIs.Analyzers/Fields/ShapeAndColorField.cs
+++ src/services/common/Codebreaker.GameAPIs.Analyzers/Fields/ShapeAndColorField.cs
@@ -2,6 +2,8 @@
 
 public partial record class ShapeAndColorField(string Shape, string Color)
 {
+    internal static ShapeAndColorField Empty => new ShapeAndColorField(string.Empty, string.Empty);
+
     private const char Separator = ';';
     public override string ToString() => $"{Shape}{Separator}{Color}";
 
