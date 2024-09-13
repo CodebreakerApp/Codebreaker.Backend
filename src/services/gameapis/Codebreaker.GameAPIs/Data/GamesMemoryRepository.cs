609f57dfb93db6b17db46675ac12c5dae830319c
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-09-13 11:41:47 +0200

    Added GameType filter to GamesMemoryRepository

--- src/services/gameapis/Codebreaker.GameAPIs/Data/GamesMemoryRepository.cs
+++ src/services/gameapis/Codebreaker.GameAPIs/Data/GamesMemoryRepository.cs
@@ -67,6 +67,11 @@ public partial class GamesMemoryRepository(ILogger<GamesMemoryRepository> logger
             filteredGames = filteredGames.Where(g => g.HasEnded());
         }
 
+        if (gamesQuery.GameType != null)
+        {
+            filteredGames = filteredGames.Where(g => g.GameType == gamesQuery.GameType);
+        }
+
         filteredGames = filteredGames.OrderBy(g => g.Duration).ThenBy(g => g.StartTime).Take(500);
 
         filteredGames = filteredGames.ToList();
